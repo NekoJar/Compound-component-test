4bf1b4e753430cadd432aee7ee575569720eb087
NekoJar <mzharfanhakim7@gmail.com>
2023-09-23 10:29:24 +0700

    creating a compound component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Counter from "./Counter";
+import "./styles.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Compound Component Pattern</h1>
+      {/* <Counter
+        iconIncrease="+"
+        iconDecrease="-"
+        label="My NOT so flexible counter"
+        hideLabel={false}
+        hideIncrease={false}
+        hideDecrease={false}
+      /> */}
+      <div>
+        <Counter>
+          <Counter.Decrease icon="-" />
+          <Counter.Count />
+          <Counter.Increase icon="+" />
+          <Counter.Label>My super flexible counter</Counter.Label>
+        </Counter>
+      </div>
     </div>
   );
 }
-
-export default App;
